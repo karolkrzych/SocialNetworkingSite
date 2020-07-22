18aafc551d0c8216103ffe6e60118537477959c2
karolkrzych <karolkrzych@gmail.com>
2020-07-22 14:25:01 +0200

    Stworzenie bazy danych

--- SocialNetworkinSite.API/Data/DataContext.cs
+++ SocialNetworkinSite.API/Data/DataContext.cs
@@ -5,8 +5,8 @@ namespace SocialNetworkinSite.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<Value> Values { get; set; }        
+        public DbSet<Value> Values { get; set; }
     }
 }
